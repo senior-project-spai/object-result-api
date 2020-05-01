237024405005231a53f2069b8e2fd3c62a3b06d6
Rawit Panjaroen <check.rawit@gmail.com>
2020-05-01 23:10:25 +0700

    fix: environment variable name

--- app/config.py
+++ app/config.py
@@ -10,6 +10,6 @@ MYSQL = {
     'host': os.environ['MYSQL_HOST'],
     'port': os.environ['MYSQL_PORT'],
     'user': os.environ['MYSQL_USER'],
-    'passwd': os.environ['PASSWORD'],
+    'passwd': os.environ['MYSQL_PASSWORD'],
     'db': os.environ['MYSQL_DB'] 
 }
